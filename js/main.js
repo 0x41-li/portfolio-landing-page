a1bffd5c052dda8fdebaf5f542c21c1799fab0b3
Ali Sbane <theirisbugs@gmail.com>
2021-04-06 08:56:52 +0100

    Adding Final Tweaks, responsiveness on all screen sizes, making the header fixed relative to the scrolling event, fixing the logo circle

--- js/main.js
+++ js/main.js
@@ -3,9 +3,24 @@
 // || VARIABLES
 const hum = document.querySelector(".hum");
 const nav = document.querySelector("nav");
+const header = document.querySelector("header");
+
+let y;
 
 // || SITEWIDE
 hum.onclick = function() {
 	hum.classList.toggle("hum-close");
 	nav.classList.toggle("nav-show");
+}
+
+document.addEventListener('scroll', scrolling);
+
+function scrolling() {
+	y = window.scrollY;
+	if(y > 52) {
+		header.classList.add("fixed");
+	}
+	else {
+		header.classList.remove("fixed");
+	}
 }
